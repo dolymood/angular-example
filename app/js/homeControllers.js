76788a507d65b96c32b149de810c28f7216052f3
dolymood <dolymood@gmail.com>
2014-01-26 12:44:04 +0800

    cc

--- app/js/homeControllers.js
+++ app/js/homeControllers.js
@@ -2,9 +2,32 @@ var homeControllers = angular.module('homeControllers', []);
 
 homeControllers
 
-.controller('indexCtl', function() {
-	// todo...
-})
+.controller('indexCtl', [
+	'$scope',
+	'$http',
+	function($scope, $http) {
+		
+		$http.get('../data/index.json').success(function(res) {
+			$scope.items = res;
+		});
+
+		var id = 11;
+		$scope.addNewItem = function() {
+			if (!$scope.newItem) return;
+			$scope.items.push({
+				id: id++,
+				name: $scope.newItem,
+				grp: 'new grp...'
+			});
+			$scope.newItem = '';
+		};
+
+		$scope.delItem = function(item) {
+			$scope.items.splice($scope.items.indexOf(item), 1);
+		};
+
+	}
+])
 
 .controller('index1Ctl', function() {
 	// todo...
